1591a668b09d582f9c59c07747f0c671c8329df4
Michelle Uzonna <mitchelleuzonna@gmail.com>
2022-12-11 23:22:35 +0100

    final commit

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,15 @@ import logger from 'morgan';
 import cors from 'cors';
 import usersRouter from './routes/usersRoute';
 // import indexRouter from '../routes/index';
-connectDb()
+
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
+
 const app = express();
+
+const swaggerDocs = YAML.load('./documentation.yaml');
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+connectDb()
 
 
 app.use(cors()).use(helmet())
