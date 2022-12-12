53760289499aa4bb159b3574e9d05e9696b971a2
Michelle Uzonna <mitchelleuzonna@gmail.com>
2022-12-12 09:40:29 +0100

    removing auth in getallusers route

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.post('/register', registerUser)
 router.post('/login', loginUser);
 router.get('/getsingleuser/:id', auth, getSingleUser);
-router.get('/getallusers', auth, getAllUsers);
+router.get('/getallusers', getAllUsers);
 router.patch('/updateuser/:id', auth,  updateUser);
 router.delete('/deleteuser/:id', auth, deleteUser);
 
